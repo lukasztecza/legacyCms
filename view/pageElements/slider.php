3554dcf5104485e77ddea0bd01f9c912c5b96ce1
lukasztecza <lukasztecza.pl@gmail.com>
2017-07-09 09:24:46 -0400

    sliders not images anymore

--- view/pageElements/slider.php
+++ view/pageElements/slider.php
@@ -10,64 +10,40 @@
     <?php break;case "sidebox": ?>
         <div id="sidebox"><div><pre style="white-space:pre-wrap;"><?php echo Dictionary::get($element["content"]); ?></pre></div></div>
     <?php break;case "facebook": ?>
-        <a id="facebook" href="<?php echo $element["content"]; ?>" target="_blank">
-            <img
-                 width="50px"
-                 height=50px"
-                 src="<?php echo Config::getDefaultSite() . "style/graphics/facebook.jpg"; ?>"
-                 alt="facebook"
-                 title="facebook"
-             />
+        <a
+            style="display:inline-block;width:50px;height:50px;background-image:url(style/graphics/facebook.jpg);"
+            id="facebook"
+            href="<?php echo $element["content"]; ?>" target="_blank">
         </a>
     <?php break;case "twitter": ?>
-        <a id="twitter" href="<?php echo $element["content"]; ?>" target="_blank">
-            <img
-                 width="50px"
-                 height=50px"
-                 src="<?php echo Config::getDefaultSite() . "style/graphics/twitter.jpg"; ?>"
-                 alt="twitter"
-                 title="twitter"
-             />
+        <a
+            style="display:inline-block;width:50px;height:50px;background-image:url(style/graphics/twitter.jpg);"
+            id="twitter"
+            href="<?php echo $element["content"]; ?>" target="_blank">
         </a>
     <?php break;case "youtube": ?>
-        <a id="youtube" href="<?php echo $element["content"]; ?>" target="_blank">
-            <img
-                 width="50px"
-                 height=50px"
-                 src="<?php echo Config::getDefaultSite() . "style/graphics/youtube.jpg"; ?>"
-                 alt="youtube"
-                 title="youtube"
-             />
+        <a
+            style="display:inline-block;width:50px;height:50px;background-image:url(style/graphics/youtube.jpg);"
+            id="youtube"
+            href="<?php echo $element["content"]; ?>" target="_blank">
         </a>
     <?php break;case "googleplus": ?>
-        <a id="googleplus" href="<?php echo $element["content"]; ?>" target="_blank">
-            <img
-                 width="50px"
-                 height=50px"
-                 src="<?php echo Config::getDefaultSite() . "style/graphics/googleplus.jpg"; ?>"
-                 alt="googleplus"
-                 title="googleplus"
-             />
+        <a
+            style="display:inline-block;width:50px;height:50px;background-image:url(style/graphics/googleplus.jpg);"
+            id="googleplus"
+            href="<?php echo $element["content"]; ?>" target="_blank">
         </a>
     <?php break;case "linkedin": ?>
-        <a id="linkedin" href="<?php echo $element["content"]; ?>" target="_blank">
-            <img
-                 width="50px"
-                 height=50px"
-                 src="<?php echo Config::getDefaultSite() . "style/graphics/linkedin.jpg"; ?>"
-                 alt="linkedin"
-                 title="linkedin"
-             />
+        <a
+            style="display:inline-block;width:50px;height:50px;background-image:url(style/graphics/linkedin.jpg);"
+            id="linkedin"
+            href="<?php echo $element["content"]; ?>" target="_blank">
         </a>
     <?php break;case "github": ?>
-        <a id="github" href="<?php echo $element["content"]; ?>" target="_blank">
-            <img
-                 width="50px"
-                 height=50px"
-                 src="<?php echo Config::getDefaultSite() . "style/graphics/github.jpg"; ?>"
-                 alt="github"
-                 title="github"
-             />
+        <a
+            style="display:inline-block;width:50px;height:50px;background-image:url(style/graphics/github.jpg);"
+            id="github"
+            href="<?php echo $element["content"]; ?>" target="_blank">
         </a>
     <?php break;endswitch; ?>
 <?php endforeach; ?>
